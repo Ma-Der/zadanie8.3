563abc0eec9e21d88f81ba24ebb890bd57ac1e17
Maciej Derewiański <maciej.derewianski@onet.eu>
2018-10-03 19:13:26 +0200

    Fix bugs

--- js/script.js
+++ js/script.js
@@ -3,28 +3,28 @@
 // Function that displays information and adds new line
 
 var returnInformation = function(info){
-  //var output = document.getElementById('output');
+  var output = document.getElementById('output');
   output.innerHTML = output.innerHTML + info + '<br><br>';
 };
 
 // Function that takes Celsius temperature and give back some fun facts about weather
 
-var funfact = function(temperatureC){
-  if(temperatureC <= 0){
+var funfact = function(fact){
+  if(fact <= 0){
     returnInformation('Wear a coat, it is cold out there.');
   }
-  else if (temperatureC > 0 && temperatureC <= 14)
+  else if (fact > 0 && fact <= 14)
     returnInformation('Spring is coming, look for lighter clothes.');
-  else if (temperatureC > 14 && temperatureC <= 25)
+  else if (fact > 14 && fact <= 25)
     returnInformation('It is becoming hotter and hotter, wear a t-shirt');
-  else if (temperatureC > 25)
+  else if (fact > 25)
     returnInformation('I am melting out here');
 };
 
 // Function Celsius to Fahrenheit
 
-var celsiusToFahrenheit = function(temperatureC) {
-  temperatureC = window.prompt('Type the temperature in Celsius');
+var celsiusToFahrenheit = function() {
+  var temperatureC = window.prompt('Type the temperature in Celsius');
 	if(isNaN(temperatureC) || temperatureC == '' || temperatureC == null){
 			returnInformation('Type a number');
 	}
@@ -37,8 +37,8 @@ var celsiusToFahrenheit = function(temperatureC) {
 
 //Function Fahrenheit to Celsiuses
 
-var fahrenheitToCelsius = function(temperatureF) {
-    temperatureF = window.prompt('Type the temperature in Fahrenheits');
+var fahrenheitToCelsius = function() {
+    var temperatureF = window.prompt('Type the temperature in Fahrenheits');
     	if(isNaN(temperatureF) || temperatureF == '' || temperatureF == null){
     			returnInformation('Type a number');
     	}
@@ -50,11 +50,7 @@ var fahrenheitToCelsius = function(temperatureF) {
 };
 
 var buttonC = document.getElementById('Celsius-button');
-buttonC.addEventListener('click', function(){
-  celsiusToFahrenheit();
-});
+buttonC.addEventListener('click', celsiusToFahrenheit);
 
 var buttonF = document.getElementById('Fahrenheit-button');
-buttonF.addEventListener('click', function(){
-	fahrenheitToCelsius();
-});
+buttonF.addEventListener('click', fahrenheitToCelsius);
